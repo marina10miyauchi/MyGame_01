4dab6368d7434c5a0e0441922489134b5b70add1
Marina_Miyauchi <mari01miya@gmail.com>
2019-09-26 18:42:04 +0900

    SoundManager,SceneManagerの作成

---
+++ MyGame/Assets/Script/Manager/Scene_Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : SingletonMonoBehaviour<Scene_Manager>
+{
+    private void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void ChangeScene(string sceneName,string bgmName)
+    {
+        ChangeScene(sceneName);
+        SoundManager.Instance.PlayBGMByName(bgmName);
+    }
+    //現在のシーンの取得
+    public Scene GetThisScene()
+    {
+        return SceneManager.GetActiveScene();
+    }
+
+}
