5b53346696f71c6a06a4beb0ab9c956391d190ad
Marina_Miyauchi <mari01miya@gmail.com>
2019-11-04 22:48:15 +0900

    3DSound用のSoundManagerの作成

---
+++ MyGame/Assets/Script/Manager/BGMManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : SingletonMonoBehaviour<BGMManager>
+{
+    AudioClip[] m_bgm;
+
+    Dictionary<string, int> m_bgmIndex = new Dictionary<string, int>();
+    AudioSource m_bgmSource;
+
+    void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        m_bgmSource = gameObject.AddComponent<AudioSource>();
+
+        m_bgm = Resources.LoadAll<AudioClip>("Sound");
+        for (int num = 0; num < m_bgm.Length; num++)
+        {
+            m_bgmIndex.Add(m_bgm[num].name, num);
+        }
+    }
+
+    //BGM再生
+    public void PlayBGM(string name)
+    {
+        PlayBGM(GetBGMIndex(name));
+    }
+    void PlayBGM(int index)
+    {
+        index = Mathf.Clamp(index, 0, m_bgm.Length);
+
+        m_bgmSource.clip = m_bgm[index];
+        m_bgmSource.loop = true;
+
+        m_bgmSource.Play();
+    }
+    int GetBGMIndex(string name)
+    {
+        if (m_bgmIndex.ContainsKey(name))
+        {
+            return m_bgmIndex[name];
+        }
+        else
+        {
+            Debug.LogError("検索された名前のBGMは存在しません。確認してください。");
+            return 0;
+        }
+    }
+    //BGM停止
+    public void StopBGM()
+    {
+        m_bgmSource.Stop();
+        m_bgmSource.clip = null;
+    }
+}
---
+++ MyGame/Assets/Script/Manager/SEManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEManager : SingletonMonoBehaviour<SEManager>
+{
+    AudioClip[] m_se;
+
+    Dictionary<string, int> m_seIndex = new Dictionary<string, int>();
+
+    GameObject m_seObj;
+
+    void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        //SE全ロード
+        m_se = Resources.LoadAll<AudioClip>("Sound");
+        for (int num = 0; num < m_se.Length; num++)
+        {
+            m_seIndex.Add(m_se[num].name, num);
+        }
+
+        //オブジェクト指定がなかった時用のSE源　生成と設定
+        {
+            m_seObj = new GameObject("SEObj");
+            m_seObj.transform.parent = transform;
+            m_seObj.gameObject.AddComponent<AudioSource>();
+            m_seObj.GetComponent<AudioSource>().spatialize = true;
+            m_seObj.GetComponent<AudioSource>().reverbZoneMix = 1.1f;
+        }
+    }
+    
+    public void PlaySE(GameObject sourceObj,string name)
+    {
+        PlaySE(sourceObj, GetSEIndex(name));
+    }
+    public void PlaySE(string name)
+    {
+        Vector3 cameraPos = Camera.main.transform.position;
+
+        m_seObj.transform.position = new Vector3(cameraPos.x,cameraPos.y,cameraPos.z+2);
+        PlaySE(m_seObj, GetSEIndex(name));
+
+    }
+    void PlaySE(GameObject sourceObj,int index)
+    {
+        index = Mathf.Clamp(index, 0, m_se.Length);
+        var audio = sourceObj.GetComponent<AudioSource>();
+        audio.PlayOneShot(m_se[index]);
+        
+    }
+    int GetSEIndex(string name)
+    {
+        if (m_seIndex.ContainsKey(name))
+        {
+            return m_seIndex[name];
+        }
+        else
+        {
+            Debug.LogError("検索された名前のSEは存在しません。確認してください。");
+            return 0;
+        }
+    }
+    public void StopSE(GameObject sourceObj)
+    {
+        var audio= sourceObj.GetComponent<AudioSource>();
+        audio.Stop();
+        audio.clip = null;
+    }
+
+}
